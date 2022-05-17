383e9d3f7718b79120c0ab19a2202b9bd79fbbb1
Vincent <vschorp@ethz.ch>
2022-05-17 20:46:19 +0200

    minor change tensorboard launcher

--- raisimGymTorch/raisimGymTorch/env/envs/rsg_ouzel_delta/scripts/tensorboard_launch.py
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg_ouzel_delta/scripts/tensorboard_launch.py
@@ -2,7 +2,5 @@ import os
 
 from raisimGymTorch.helper.raisim_gym_helper import ConfigurationSaver, tensorboard_launcher
 
-tensorboard_launcher(
-    "/home/vincent/rl_4_aerial_manipulator/catkin_ws/src/raisimLib/raisimGymTorch/data/ouzel_delta_planning"
-)
+tensorboard_launcher("/home/vincent/rl_4_aerial_manipulator/catkin_ws/src/raisimLib/raisimGymTorch/data/ouzel_delta_planning")
 # tensorboard_launcher("/home/vincent/ouzel_delta_planning")
